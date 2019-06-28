6ec49a6bb5ea46649a6718087850547557fb1c8c
rino <rinovethamoses97@gmail.com>
2019-06-28 23:48:50 +0530

    game play improvements

--- public/sketch.js
+++ public/sketch.js
@@ -3,7 +3,7 @@ var invaders=[];
 var key=0;
 var shootSound;
 var invaderRate=200;
-var productionRate=3;
+var productionRate=3;//ranges from 1 to 10 for the current canvas width
 function preload(){
     shootSound=loadSound("./gunsound.mp3");
 }
@@ -15,12 +15,12 @@ function draw(){
     background(0);
     if(frameCount%invaderRate==0){
         for(var i=0;i<productionRate;i++){
-            var invader=new Invader(random(width-60),0);
+            var invader=new Invader(random((i)*(width/productionRate),(i+1)*(width/productionRate)-50,0));
             invader.appleForce(2);
             this.invaders.push(invader);
             invaderRate-=1;
         }
-        productionRate+=0.2;
+        // productionRate+=0.2;
     }
     player.show();
     player.updateBombs();
